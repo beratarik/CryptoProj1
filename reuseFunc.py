9511b17b44b6405cbf6e0353df37e0bebb3117f3
pgensheimer <rxz142@vols.utk.edu>
2017-10-04 11:35:40 -0400

    added implementation of AES enc and Dec

--- reuseFunc.py
+++ reuseFunc.py
@@ -14,5 +14,18 @@ def readInputs(iname, kname, oname,vname):
 			oname = sys.argv[count+1]
 		elif(sys.argv[count] == "-v")
 			vname = sys.argv[count+1]
-		count = count+2
+		count = count+2;
 	return kname, iname, oname, vname
+
+def impAESenc(key, inputString)
+	#inputstring should be 8 bytes
+	
+	cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+	ctext = cipher.encrypt(inputString)
+	return binascii.hexlify(bytearray(ctext)).decode('utf-8')
+	
+def impAESdec(key, inputString)
+	enc = binascii.unhexlify(enc)
+	cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+	enc = cipher.decrypt(enc)
+	return enc.decod('utf-8')
