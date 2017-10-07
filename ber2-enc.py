8d24e929b01c93db93165127e21dba16d87f489d
pgensheimer <rxz142@vols.utk.edu>
2017-10-06 21:28:26 -0400

    took out printing, added in reading in iv

--- ber2-enc.py
+++ ber2-enc.py
@@ -1,46 +1,43 @@
 import sys
 import binascii as ba
 from Crypto.Cipher import AES
-from Crypto import Random
 from Crypto.Util import strxor
 import random
 from reuseFunc import readInputs
 def main():
     kname, iname, oname, vname = readInputs(sys.argv[1:])
-    #kname = sys.argv[1]
-    #iname = sys.argv[2]
-    #oname = sys.argv[3]
+   
 
     blocksize = 8
     l = []
-
-    if(len(sys.argv) == 5):
-        ivname = sys.argv[4]
-        ivfile = open(ivname, 'r')
-
+    isIV = 0
+    if vname != '':
+        vfile = open(vname, 'r')
+        isIV = 1
     kfile = open(kname, 'r')
     ifile = open(iname, 'r')
     ofile = open(oname, 'w')
 
     message = ifile.read()
     message = message[:-1]
-    #message = message.encode('utf-8')
 
     key = kfile.read()
     key = key[:-1]
 
     ciphertext =''
-    rndfile = Random.new()
-    IV = rndfile.read(8)
-    ran = random.randrange(10**80)
-    myhex = "%64x" %ran
-    myhex = myhex[:16]
+    if isIV == 0:
+        ran = random.randrange(10**80)
+        myhex = "%64x" %ran
+        myhex = myhex[:16]
+    else:
+        myhex = vfile.read()
+        myhex = myhex[:-1]
+        myhex = myhex[:16]
     ciphertext += myhex
     myhex = bytes(myhex, 'utf-8')
-    print("myhex is " + str(myhex))
-    print("IV is " + str(IV))
-    IV = ba.hexlify(IV)
-    print("IV is  " + str(IV))
+    #print("myhex is " + str(myhex))
+
+   
     #print("Unpadded message is " + message)
     padded = pad(message)
 
@@ -50,27 +47,27 @@ def main():
         if(i%blocksize == blocksize-1 and i != 0):
             l.append(padded[i-blocksize+1:i+1])
     
-    for i in range(len(l)):
-        print("hex is " + str((l[i])))
+    #for i in range(len(l)):
+        #print("hex is " + str((l[i])))
     
-    print("l is " + str(ba.hexlify(l[0]))) 
+    #print("l is " + str(ba.hexlify(l[0]))) 
     c = strxor.strxor(myhex, ba.hexlify(l[0]))
-    h = strxor.strxor(myhex, c)
-    print("h is " +str((h)))
+    #h = strxor.strxor(myhex, c)
+    #print("h is " +str((h)))
     #c = xor(myhex, (l[0]))
-    print("C is " +str(ba.hexlify(c)))
+    #print("C is " +str(ba.hexlify(c)))
     
     cipher = encrypt(key, c)
     ciphertext = createCipher(ciphertext, cipher)
-    print(ciphertext)
+    #print(ciphertext)
     for i in range(1,len(l)):
-        print("xor of " + str(ba.unhexlify(cipher)) + " and " + str(ba.hexlify(l[i])))
+        #print("xor of " + str(ba.unhexlify(cipher)) + " and " + str(ba.hexlify(l[i])))
         c = strxor.strxor(ba.unhexlify(cipher),ba.hexlify(l[i]))
-        print("Xor res is " + str(c))
+        #print("Xor res is " + str(c))
         cipher = encrypt(key, c)
         ciphertext = createCipher(ciphertext, cipher)
 
-    print(ciphertext)
+    #print(ciphertext)
     ofile.write(ciphertext)
 
 def pad(message):
@@ -80,28 +77,7 @@ def pad(message):
     padded = (message + padbyte * padamount).encode('utf-8')
     return padded
 
-def xor(xor_val,mess_block):
-    print("xor of " + str(xor_val) + " and " + str(mess_block))
-    #return ba.hexlify(bytes(a^b) for a, b in zip(ba.unhexlify(xor_val), ba.unhexlify(mess_block)))
-    #encrypt = int(xor_val, 16) ^ int(mess_block, 16)
-    #for i in range(len(encrypt)):
-    #    c += str(encrypt[i])
-    #c = ''.join(str(encrypt))
-    #return hex(encrypt)[2:]
-    #encrypted = [ chr(a ^ b) for (a,b) in zip(xor_val, mess_block) ]
-    #print(encrypted)
-    #str1 = ''.join(encrypted)
-    #str1 = str1.encode('utf-8')
-    #print(str1)
-    b = xor_val
-    if type(xor_val) is str:
-        b = bytes(xor_val,encoding='utf8')
-    iData = int.from_bytes(mess_block, sys.byteorder)
-    iKey = int.from_bytes(b, sys.byteorder)
-    xored = iData ^ iKey
-    xored - xored.to_bytes(len(mess_block), sys.byteorder)
-    return xored
-    #return str1
+
 def encrypt(key, message):
     cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
     ciphertext = cipher.encrypt(message)
